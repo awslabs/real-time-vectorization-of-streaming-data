85a50b2c0cdf68d1b2d41b97e13d49ac2702a75f
Melissa Wang <meliwang@amazon.com>
2024-12-15 17:51:41 -0800

    test commit

--- src/test/java/com/amazonaws/datastreamvectorization/integrationtests/OpenSearchRestClient.java
+++ src/test/java/com/amazonaws/datastreamvectorization/integrationtests/OpenSearchRestClient.java
@@ -9,16 +9,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequestInterceptor;
 import org.junit.jupiter.api.Assertions;
+import org.opensearch.client.RequestOptions;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestClientBuilder;
 import org.opensearch.client.RestHighLevelClient;
-import org.opensearch.client.json.JsonData;
-import org.opensearch.client.opensearch.OpenSearchClient;
-import org.opensearch.client.opensearch._types.mapping.*;
-import org.opensearch.client.opensearch.core.SearchResponse;
-import org.opensearch.client.opensearch.indices.*;
-import org.opensearch.client.transport.aws.AwsSdk2Transport;
-import org.opensearch.client.transport.aws.AwsSdk2TransportOptions;
+//import org.opensearch.client.json.JsonData;
+//import org.opensearch.client.opensearch.OpenSearchClient;
+//import org.opensearch.client.opensearch._types.mapping.*;
+//import org.opensearch.client.opensearch.core.SearchResponse;
+//import org.opensearch.client.opensearch.indices.*;
+//import org.opensearch.client.transport.aws.AwsSdk2Transport;
+//import org.opensearch.client.transport.aws.AwsSdk2TransportOptions;
+import org.opensearch.client.indices.CreateIndexRequest;
+import org.opensearch.client.indices.CreateIndexResponse;
+import org.opensearch.common.settings.Settings;
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.signer.Aws4Signer;
 import software.amazon.awssdk.http.SdkHttpClient;
@@ -37,111 +41,111 @@ public class OpenSearchRestClient {
         region = regionProvider.getRegion();
     }
 
-//    public CreateIndexResponse createIndex(String openSearchEndpoint, OpenSearchType openSearchType, String indexName, EmbeddingModel embeddingModel) {
-//        try (RestHighLevelClient client = getRestHighLevelClient(openSearchEndpoint, openSearchType)) {
-//            // create mappings for index
-//            Map<String, Object> parameters = new HashMap<>();
-//            parameters.put("ef_construction", 128);
-//            parameters.put("m", 24);
-//
-//            Map<String, Object> method = new HashMap<>();
-//            method.put("name", "hnsw");
-//            method.put("engine", "nmslib");
-//            method.put("parameters", parameters);
-//
-//            Map<String, Object> embeddedData = new HashMap<>();
-//            embeddedData.put("type", "knn_vector");
-//            embeddedData.put("dimension", embeddingModel.getModelDefaultDimensions());
-//            embeddedData.put("method", method);
-//
-//            // build the create index request
-//            CreateIndexRequest createIndexRequest = new CreateIndexRequest(indexName);
-//            createIndexRequest.settings(Settings.builder()
-//                    .put("index.knn", true)
-//            );
-//            createIndexRequest.mapping(embeddedData);
-//
-//            // send the create index request
-//            return client.indices().create(createIndexRequest, RequestOptions.DEFAULT);
-//        } catch (Exception e) {
-//            throw new RuntimeException("Error when creating the OpenSearch index" + indexName + ": ", e);
-//        }
-//    }
-
     public CreateIndexResponse createIndex(String openSearchEndpoint, OpenSearchType openSearchType, String indexName, EmbeddingModel embeddingModel) {
-        try (SdkHttpClient httpClient = createHttpClient()) {
-            // create settings for index
-            IndexSettings settings = new IndexSettings.Builder()
-                    .knn(true)
-                    .build();
+        try (RestHighLevelClient client = getRestHighLevelClient(openSearchEndpoint, openSearchType)) {
+            // create mappings for index
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("ef_construction", 128);
+            parameters.put("m", 24);
 
-            // https://github.com/opensearch-project/opensearch-java/blob/main/java-client/src/test/java/org/opensearch/client/opensearch/_types/mapping/KnnVectorMethodTest.java
-            Map<String, JsonData> parameters = new HashMap<>();
-            parameters.put("ef_construction", JsonData.of(128));
-            parameters.put("m", JsonData.of(24));
+            Map<String, Object> method = new HashMap<>();
+            method.put("name", "hnsw");
+            method.put("engine", "nmslib");
+            method.put("parameters", parameters);
 
-            KnnVectorMethod method = new KnnVectorMethod.Builder()
-                    .name("hnsw")
-                    .engine("nmslib")
-                    .parameters(parameters)
-                    .build();
-            KnnVectorProperty embeddedData = new KnnVectorProperty.Builder()
-                    .dimension(embeddingModel.getModelDefaultDimensions())
-                    .method(method)
-                    .build();
-
-            Map<String, Property> properties = new HashMap<>();
-            properties.put("embedded_data", new Property(embeddedData));
-
-            TypeMapping mappings = new TypeMapping.Builder()
-                    .properties(properties)
-                    .build();
+            Map<String, Object> embeddedData = new HashMap<>();
+            embeddedData.put("type", "knn_vector");
+            embeddedData.put("dimension", embeddingModel.getModelDefaultDimensions());
+            embeddedData.put("method", method);
 
             // build the create index request
-            CreateIndexRequest createIndexRequest = new CreateIndexRequest.Builder()
-                    .index(indexName)
-                    .settings(settings)
-                    .mappings(mappings)
-                    .build();
+            CreateIndexRequest createIndexRequest = new CreateIndexRequest(indexName);
+            createIndexRequest.settings(Settings.builder()
+                    .put("index.knn", true)
+            );
+            createIndexRequest.mapping(embeddedData);
 
             // send the create index request
-            OpenSearchClient client = createOpenSearchClient(httpClient, openSearchEndpoint, openSearchType);
-            return client.indices().create(createIndexRequest);
+            return client.indices().create(createIndexRequest, RequestOptions.DEFAULT);
         } catch (Exception e) {
             throw new RuntimeException("Error when creating the OpenSearch index" + indexName + ": ", e);
         }
     }
 
-    public DeleteIndexResponse deleteIndex(String openSearchEndpoint, OpenSearchType openSearchType, String indexName) {
-        try (SdkHttpClient httpClient = createHttpClient()) {
-            DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest.Builder()
-                    .index(indexName)
-                    .build();
-            OpenSearchClient client = createOpenSearchClient(httpClient, openSearchEndpoint, openSearchType);
-            return client.indices().delete(deleteIndexRequest);
-        } catch (Exception e) {
-            throw new RuntimeException("Error when deleting the OpenSearch index" + indexName + ": ", e);
-        }
-    }
+//    public CreateIndexResponse createIndex(String openSearchEndpoint, OpenSearchType openSearchType, String indexName, EmbeddingModel embeddingModel) {
+//        try (SdkHttpClient httpClient = createHttpClient()) {
+//            // create settings for index
+//            IndexSettings settings = new IndexSettings.Builder()
+//                    .knn(true)
+//                    .build();
+//
+//            // https://github.com/opensearch-project/opensearch-java/blob/main/java-client/src/test/java/org/opensearch/client/opensearch/_types/mapping/KnnVectorMethodTest.java
+//            Map<String, JsonData> parameters = new HashMap<>();
+//            parameters.put("ef_construction", JsonData.of(128));
+//            parameters.put("m", JsonData.of(24));
+//
+//            KnnVectorMethod method = new KnnVectorMethod.Builder()
+//                    .name("hnsw")
+//                    .engine("nmslib")
+//                    .parameters(parameters)
+//                    .build();
+//            KnnVectorProperty embeddedData = new KnnVectorProperty.Builder()
+//                    .dimension(embeddingModel.getModelDefaultDimensions())
+//                    .method(method)
+//                    .build();
+//
+//            Map<String, Property> properties = new HashMap<>();
+//            properties.put("embedded_data", new Property(embeddedData));
+//
+//            TypeMapping mappings = new TypeMapping.Builder()
+//                    .properties(properties)
+//                    .build();
+//
+//            // build the create index request
+//            CreateIndexRequest createIndexRequest = new CreateIndexRequest.Builder()
+//                    .index(indexName)
+//                    .settings(settings)
+//                    .mappings(mappings)
+//                    .build();
+//
+//            // send the create index request
+//            OpenSearchClient client = createOpenSearchClient(httpClient, openSearchEndpoint, openSearchType);
+//            return client.indices().create(createIndexRequest);
+//        } catch (Exception e) {
+//            throw new RuntimeException("Error when creating the OpenSearch index" + indexName + ": ", e);
+//        }
+//    }
 
-    public List<OpenSearchIndexDocument> queryIndexRecords(OpenSearchType openSearchType, String indexName, String openSearchEndpoint) {
-        // TODO: https://opensearch.org/docs/latest/clients/java-rest-high-level/
-        //      The OpenSearch Java high-level REST client is deprecated.
-        //      Support will be removed in OpenSearch version 3.0.0.
-        //      We recommend switching to the Java client instead.
-        //  SWITCH create index to below method with Java client
+//    public DeleteIndexResponse deleteIndex(String openSearchEndpoint, OpenSearchType openSearchType, String indexName) {
+//        try (SdkHttpClient httpClient = createHttpClient()) {
+//            DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest.Builder()
+//                    .index(indexName)
+//                    .build();
+//            OpenSearchClient client = createOpenSearchClient(httpClient, openSearchEndpoint, openSearchType);
+//            return client.indices().delete(deleteIndexRequest);
+//        } catch (Exception e) {
+//            throw new RuntimeException("Error when deleting the OpenSearch index" + indexName + ": ", e);
+//        }
+//    }
 
-        // https://opensearch.org/docs/latest/clients/java/#connecting-to-amazon-opensearch-service
-        try (SdkHttpClient httpClient = createHttpClient()) {
-            OpenSearchClient client = createOpenSearchClient(httpClient, openSearchEndpoint, openSearchType);
-                    // https://opensearch.org/docs/latest/clients/java/#searching-for-documents
-                    SearchResponse<OpenSearchIndexDocument> searchResponse = client.search(
-                    s -> s.index(indexName), OpenSearchIndexDocument.class);
-            return searchResponse.documents();
-        } catch (Exception e) {
-            throw new RuntimeException("Error when searching the index " + indexName + ": ", e);
-        }
-    }
+//    public List<OpenSearchIndexDocument> queryIndexRecords(OpenSearchType openSearchType, String indexName, String openSearchEndpoint) {
+//        // TODO: https://opensearch.org/docs/latest/clients/java-rest-high-level/
+//        //      The OpenSearch Java high-level REST client is deprecated.
+//        //      Support will be removed in OpenSearch version 3.0.0.
+//        //      We recommend switching to the Java client instead.
+//        //  SWITCH create index to below method with Java client
+//
+//        // https://opensearch.org/docs/latest/clients/java/#connecting-to-amazon-opensearch-service
+//        try (SdkHttpClient httpClient = createHttpClient()) {
+//            OpenSearchClient client = createOpenSearchClient(httpClient, openSearchEndpoint, openSearchType);
+//                    // https://opensearch.org/docs/latest/clients/java/#searching-for-documents
+//                    SearchResponse<OpenSearchIndexDocument> searchResponse = client.search(
+//                    s -> s.index(indexName), OpenSearchIndexDocument.class);
+//            return searchResponse.documents();
+//        } catch (Exception e) {
+//            throw new RuntimeException("Error when searching the index " + indexName + ": ", e);
+//        }
+//    }
 
     // TODO: move this to a new "integ test base" class
     public void validateOpenSearchRecords(List<String> expectedOriginalDataList, List<OpenSearchIndexDocument> searchResult) {
@@ -164,17 +168,17 @@ public class OpenSearchRestClient {
         return ApacheHttpClient.builder().build();
     }
 
-    private OpenSearchClient createOpenSearchClient(SdkHttpClient httpClient, String openSearchEndpoint, OpenSearchType openSearchType) {
-        return new OpenSearchClient(
-                new AwsSdk2Transport(
-                        httpClient,
-                        openSearchEndpoint,
-                        openSearchType.getServiceName(),
-                        Region.of(this.region),
-                        AwsSdk2TransportOptions.builder().build()
-                )
-        );
-    }
+//    private OpenSearchClient createOpenSearchClient(SdkHttpClient httpClient, String openSearchEndpoint, OpenSearchType openSearchType) {
+//        return new OpenSearchClient(
+//                new AwsSdk2Transport(
+//                        httpClient,
+//                        openSearchEndpoint,
+//                        openSearchType.getServiceName(),
+//                        Region.of(this.region),
+//                        AwsSdk2TransportOptions.builder().build()
+//                )
+//        );
+//    }
 
     // TODO: think about using the following instead (above method):
     //  https://opensearch.org/docs/latest/clients/java/#connecting-to-amazon-opensearch-service
