47afd99a59bc76a7474f54e9c41b2324badadcaa
Melissa Wang <meliwang@amazon.com>
2025-01-02 13:09:53 -0800

    test push integration 5

--- src/test/java/com/amazonaws/datastreamvectorization/integrationtests/BlueprintIT.java
+++ src/test/java/com/amazonaws/datastreamvectorization/integrationtests/BlueprintIT.java
@@ -18,7 +18,7 @@ import static com.amazonaws.datastreamvectorization.integrationtests.constants.I
 public class BlueprintIT {
     @Test
     void runTests() {
-        System.out.println("Print line for testing GitHub trigger integration: 4");
+        System.out.println("Print line for testing GitHub trigger integration: 5");
         // TODO: handle test parallelization later
         IntegTestCaseInput[] testInputs = this.readTestConfigFile().getTestCases();
         for (IntegTestCaseInput testCase : testInputs) {
